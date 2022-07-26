06cafe3fad01dd47845b083a695ab6d71d3d0a6e
lvidal1 <lvidal910@gmail.com>
2022-07-25 21:56:48 -0500

    [Deployment] Add .env config & deploy bank script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: `${process.env.POKT_RINKEBY_URL}`,
+      accounts: [`${process.env.RINKEBY_PRIVATE_KEY}`],
+    }
+  }
 };
---
+++ scripts/deploy-bank.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+
+    const [owner] = await hre.ethers.getSigners();
+    const BankContractFactory = await hre.ethers.getContractFactory("Bank");
+    const BankContract = await BankContractFactory.deploy();
+    await BankContract.deployed();
+
+    console.log("BankContract deployed to:", BankContract.address);
+    console.log("BankContract owner address:", owner.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    })
